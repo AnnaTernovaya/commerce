61a0843347a90d14be69c2699acaa89ea1d2bc9c
AnnaTernovaya <41382684+AnnaTernovaya@users.noreply.github.com>
2024-07-01 11:07:16 +0100

    Update sql_question_3.sql

--- models/marts/sql_answers/sql_question_3.sql
+++ models/marts/sql_answers/sql_question_3.sql
@@ -1,15 +1,15 @@
 {{ config(materialized = 'table') }}
 
--- Step 1: Identify the top 3 product categories with the most items sold in November 2017
+-- Identify the top 3 product categories with the most items sold in November 2017
 WITH top_categories AS (
   SELECT
     p.product_category_name,
     SUM(oi.price) AS total_gmv,
     COUNT(oi.order_item_id) AS items_sold
-  FROM order_items AS oi
-  JOIN orders AS o 
+  FROM {{ ref('stg_order_items') }} AS oi
+  JOIN {{ ref('stg_orders') }} AS o 
   ON oi.order_id = o.order_id
-  JOIN products AS p 
+  JOIN {{ ref('stg_order_products') }} AS p 
   ON oi.product_id = p.product_id
   WHERE EXTRACT(YEAR FROM o.order_purchase_timestamp) = 2017
     AND EXTRACT(MONTH FROM o.order_purchase_timestamp) = 11
@@ -18,24 +18,24 @@ WITH top_categories AS (
   LIMIT 3
 ),
 
--- Step 2: Calculate weekly GMV and running sum for each top category
+-- Calculate weekly GMV and running sum for each top category
 cte_weekly_GMV AS (
   SELECT
     p.product_category_name,
     EXTRACT(WEEK FROM o.order_purchase_timestamp) AS week,
     EXTRACT(YEAR FROM o.order_purchase_timestamp) AS year,
     SUM(oi.price) AS weekly_gmv
-  FROM Order_Items  AS oi
-  JOIN Orders AS o 
+  FROM {{ ref('stg_order_items') }} AS oi
+  JOIN {{ ref('stg_orders') }} AS o 
   ON oi.order_id = o.order_id
-  JOIN Products AS p
+  JOIN {{ ref('stg_products') }} AS p
   ON oi.product_id = p.product_id
   WHERE EXTRACT(YEAR FROM o.order_purchase_timestamp) = 2017
     AND p.product_category_name IN (SELECT product_category_name FROM top_categories)
   GROUP BY p.product_category_name, year, week
 ),
 
--- Step 3: Calculate running sum of GMV and weekly GMV growth rate
+-- Calculate running sum of GMV and weekly GMV growth rate
 running_sum_growth_rate AS (
   SELECT
     product_category_name,
@@ -48,7 +48,7 @@ running_sum_growth_rate AS (
   FROM cte_weekly_GMV
 )
 
--- Step 4: Select the results
+-- Select the results
 SELECT
   product_category_name,
   year,
