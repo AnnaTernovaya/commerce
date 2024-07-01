1d27519296b3049ccc10b94bce9cf33e4ae22f5f
AnnaTernovaya <41382684+AnnaTernovaya@users.noreply.github.com>
2024-07-01 11:03:54 +0100

    Update sql_question_2.sql

--- models/marts/sql_answers/sql_question_2.sql
+++ models/marts/sql_answers/sql_question_2.sql
@@ -1,6 +1,6 @@
 {{ config(materialized = 'table') }}
 
-WITH OrderData AS (
+WITH order_data AS (
   SELECT
     oi.order_id,
     oi.seller_id,
@@ -8,12 +8,12 @@ WITH OrderData AS (
     EXTRACT(MONTH FROM o.order_purchase_timestamp) AS month,
     EXTRACT(WEEK FROM o.order_purchase_timestamp) AS week,
     EXTRACT(DAY FROM o.order_purchase_timestamp) AS day
-  FROM Order_Items AS oi
-  JOIN Orders AS o ON oi.order_id = o.order_id
+  FROM {{ ref('stg_order_items') }} AS oi
+  JOIN {{ ref('stg_orders') }} AS o ON oi.order_id = o.order_id
   WHERE EXTRACT(YEAR FROM o.order_purchase_timestamp) = 2017
 ),
 
-SellerActivity AS (
+seller_activity AS (
   SELECT
     order_id,
     seller_id,
@@ -24,11 +24,11 @@ SellerActivity AS (
     COUNT(ORDER_ID) OVER (PARTITION BY seller_id, year, month) AS monthly_order_count,
     COUNT(ORDER_ID) OVER (PARTITION BY seller_id, year, month, week) AS weekly_order_count,
     COUNT(ORDER_ID) OVER (PARTITION BY seller_id, year, month, week, day) AS daily_order_count
-  FROM OrderData
+  FROM order_data
 ),
 
 -- Filter based on the required criteria for active sellers
-ActiveSellers AS (
+active_sellers AS (
   SELECT
     year,
     month,
@@ -36,18 +36,18 @@ ActiveSellers AS (
     IF(monthly_order_count >= 25, 1, 0) AS is_monthly_active,
     IF(weekly_order_count >= 5, 1, 0) AS is_weekly_active,
     IF(daily_order_count >= 1, 1, 0) AS is_daily_active
-  FROM SellerActivity
+  FROM seller_activity
 ),
 
 -- Summarize the counts for each seller
-Summary AS (
+summary AS (
   SELECT
     year,
     month,
     COUNT(DISTINCT IF(is_monthly_active = 1, seller_id, NULL)) AS monthly_active_sellers,
     AVG(COUNT(DISTINCT IF(is_weekly_active = 1, seller_id, NULL))) OVER (PARTITION BY year, month) AS avg_weekly_active_sellers,
     AVG(COUNT(DISTINCT IF(is_daily_active = 1, seller_id, NULL))) OVER (PARTITION BY year, month) AS avg_daily_active_sellers
-  FROM ActiveSellers
+  FROM active_sellers
   GROUP BY year, month
 )
 
